3bd4311c874794414b26ae21b2bf97431d0e492d
przem14 <przem.wawrzyniak@gmail.com>
2019-06-29 18:04:44 +0200

    Adds missing files

---
+++ Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject cloudPrefab;
+    [SerializeField] GameObject cloudsParent;
+
+    [Header("Spawning time")]
+    [Tooltip("In seconds")]
+    [SerializeField] float minTimeBetweenSpawns = 1f;
+    [SerializeField] float maxTimeBetweenSpawns = 1f;
+
+    [Header("Spawing position")]
+    [Tooltip("Maximum value of random ofsset which will be added to," +
+    	" or substracted from, generator position to set cloud position")]
+    [SerializeField] float maxOffseet = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnClouds());
+    }
+
+    private IEnumerator SpawnClouds() 
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(
+                Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns));
+
+            SpawnCloud();
+        }
+    }
+
+    private void SpawnCloud()
+    {
+        var offsetX = Random.Range(-maxOffseet, maxOffseet);
+        var position = transform.position + new Vector3(offsetX, 0f, 0f);
+        var cloud = Instantiate(cloudPrefab, position, Quaternion.identity);
+
+        if (cloudsParent)
+        {
+            cloud.transform.SetParent(cloudsParent.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] float minSizeFactor = 1f;
+    [SerializeField] float maxSizeFactor = 1f;
+
+    int spriteIndex;
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        SetSprite(sprites[Random.Range(0, sprites.Length)]);
+        SetScale();
+    }
+
+    private void SetScale()
+    {
+        float sizeFactor = Random.Range(minSizeFactor, maxSizeFactor);
+        transform.localScale = Vector2.one * sizeFactor;
+    }
+
+    private void SetSprite(Sprite sprite)
+    {
+        if (spriteRenderer)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+        else
+        {
+            Debug.LogWarning("Sprite renderer is not present in"
+                + " " + gameObject.name + " but "
+                + GetType().Name + " needs it.");
+        }
+    }
+}
