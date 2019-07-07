d32da12d6e08b2aa57bd70c2782265f02f14e9f0
przem14 <przem.wawrzyniak@gmail.com>
2019-07-07 13:26:44 +0200

    Adjusts player movement and adds physics material to planks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float walkVelocity;
-    [SerializeField] float walkAcceleration;
-    [SerializeField] float walkDeceleration;
-    [SerializeField] float airAcceleration;
     [SerializeField] float jumpHeight;
 
     Rigidbody2D rigidbody;
@@ -24,32 +21,15 @@ public class Player : MonoBehaviour
     {
         var horizontalFactor = Input.GetAxis("Horizontal");
 
-        var acceleration = isJumping ? airAcceleration : walkAcceleration;
-        var deceleration = isJumping ? 0f : walkDeceleration;
+        rigidbody.velocity = new Vector2(walkVelocity * horizontalFactor, rigidbody.velocity.y);
 
-        if (!Mathf.Approximately(0f, horizontalFactor))
-        {
-            var velocity = Mathf.MoveTowards(
-                rigidbody.velocity.x,
-                walkVelocity * horizontalFactor,
-                acceleration * Time.deltaTime);
-            rigidbody.velocity = new Vector2(velocity, rigidbody.velocity.y);
-        }
-        else
-        {
-            var velocity = Mathf.MoveTowards(
-                rigidbody.velocity.x,
-                0f,
-                deceleration * Time.deltaTime);
-            rigidbody.velocity = new Vector2(velocity, rigidbody.velocity.y);
-        }
 
         if (!isJumping && !Mathf.Approximately(0f, Input.GetAxis("Jump")))
         {
             isJumping = true;
             rigidbody.velocity = new Vector2(
                 rigidbody.velocity.x,
-                Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y)));
+                Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y * rigidbody.gravityScale)));
         }
 
         if (GetComponent<BoxCollider2D>().IsTouchingLayers(LayerMask.GetMask("Planks")))
